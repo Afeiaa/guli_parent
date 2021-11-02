57e0a55717248b34a70405c68956dfcb9cbb32ea
王若飞 <wangruofei@eccom.com.cn>
2021-11-02 17:57:48 +0800

    测试

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,15 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
+
+    @Autowired
+    private EduTeacherService eduTeacherService;
+
+    @GetMapping("/findall")
+    public List<EduTeacher> findAll() {
+        List<EduTeacher> list = eduTeacherService.list(null);
+        return list;
+    }
+
 
 }
 
