bbd9489bb0e4fa7f53d72c07b029e67b482080af
王若飞 <wangruofei@eccom.com.cn>
2021-11-05 17:40:03 +0800

    days 6-1

---
+++ service/service_oss/src/main/java/com/atguigu/oss/service/impl/OssServiceImpl.java
@@ -0,0 +1,49 @@
+package com.atguigu.oss.service.impl;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.atguigu.oss.service.OssService;
+import com.atguigu.oss.utils.ConstantPropertiesUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class OssServiceImpl implements OssService {
+    @Override
+    public String uploadFileAvatar(MultipartFile file) {
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = ConstantPropertiesUtils.END_POINT;
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = ConstantPropertiesUtils.ACCESS_KEY_ID;
+        String accessKeySecret = ConstantPropertiesUtils.ACCESS_KEY_ID;
+        String bucketName = ConstantPropertiesUtils.BUCKET_NAME;
+        System.out.println("***********************************" + endpoint);
+
+        OSS ossClient = null;
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = null;
+        try {
+            // 创建OSSClient实例。
+            ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+            inputStream = file.getInputStream();
+
+            String fileName = file.getOriginalFilename();
+            // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+            ossClient.putObject(bucketName, fileName, inputStream);
+            String url = "https://" + bucketName + "." + endpoint + "/" + fileName;
+            System.out.println("***********************************" + url);
+            return url;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            // 关闭OSSClient。
+            ossClient.shutdown();
+        }
+    }
+}
