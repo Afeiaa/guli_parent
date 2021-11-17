49a901bf94214b811972c5648d634d62053b3ab6
王若飞 <wangruofei@eccom.com.cn>
2021-11-17 16:27:14 +0800

    days 13-1

--- service/service_cms/src/main/java/com/atguigu/cms/service/impl/CrmBannerServiceImpl.java
+++ service/service_cms/src/main/java/com/atguigu/cms/service/impl/CrmBannerServiceImpl.java
@@ -4,7 +4,7 @@ import com.atguigu.cms.entity.CrmBanner;
 import com.atguigu.cms.mapper.CrmBannerMapper;
 import com.atguigu.cms.service.CrmBannerService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.boot.Banner;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +12,8 @@ import java.util.List;
 
 @Service
 public class CrmBannerServiceImpl extends ServiceImpl<CrmBannerMapper, CrmBanner> implements CrmBannerService {
+
+    @Cacheable(value = "banner", key = "'selectIndexList'")    // banner::selectIndexList
     @Override
     public List<CrmBanner> selectAllBanner() {
         List<CrmBanner> crmBannerList = baseMapper.selectList(null);
